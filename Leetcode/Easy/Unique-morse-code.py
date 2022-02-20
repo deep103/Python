a860cf7b5985a0c379a02c224016a3a035c9e5a0
deep103 <75354145+deep103@users.noreply.github.com>
2022-02-20 14:43:35 +0530

    Add files via upload

---
+++ Leetcode/Easy/Unique-morse-code.py
@@ -0,0 +1,51 @@
+'''
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+ 
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+Example 2:
+
+Input: words = ["a"]
+Output: 1
+'''
+
+class Solution():
+    def uniqueMorseRepresentations(self, words):
+        code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        temp = "abcdefghijklmnopqrstuvwxyz"
+        temp_dict = {}
+        final = []
+        combo = ""
+        for i in range(len(code)):
+            temp_dict[temp[i]] = code[i]
+        for word in words:
+            for i in word:
+                if i in temp_dict.keys():
+                    combo += temp_dict[i]
+            final.append(combo)
+            combo = ""
+        return len(set(final))
+words = ["gin","zen","gig","msg"]
+obj = Solution()
+print(obj.uniqueMorseRepresentations(words))
